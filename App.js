a8b90ab0cec39f91d1724820e7f5634fbbcc0b4e
crimsoftit <crimsoftit@gmail.com>
2021-03-12 10:42:36 +0300

    1. basic qrcode scanner

--- App.js
+++ App.js
@@ -1,28 +1,117 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import {
+	SafeAreaView,
+	StyleSheet,
+	ScrollView,
+	View,
+	Text,
+	StatusBar,
+	TouchableOpacity,
+	Linking
+} from 'react-native';
+
+import {
+	Header,
+	LearnMoreLinks,
+	Colors,
+	DebugInstructions,
+	ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import React, { Component } from 'react';
+
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import { RNCamera } from 'react-native-camera';
+import QRCode from 'react-native-qrcode-svg';
+
+export default class App extends Component {
+
+	state = {
+		qr: ""
+	}
+
+	onRead = e => {
+		this.setState({ qr: e.data })
+		//console.log(e);
+	}
+	render() {
+		return (
+		<>
+			<StatusBar barStyle="dark-content" />
+			<SafeAreaView>
+				<ScrollView
+					contentInsetAdjustmentBehavior="automatic"
+					style={styles.scrollView}>
+
+					<QRCodeScanner
+						onRead={this.onRead}
+					/>
+
+					{this.state.qr ? <QRCode 
+						value={this.state.qr}
+					/> : null}
+					
+
+
+					{global.HermesInternal == null ? null : (
+						<View style={styles.engine}>
+							<Text style={styles.footer}>Engine: Hermes</Text>
+						</View>
+					)}
+
+
+					
+				
+				</ScrollView>
+			</SafeAreaView>
+		</>
+		)
+	}
+}
+
 
 const styles = StyleSheet.create({
-  center: {
-    alignItems: 'center'
-  }
-})
-
-const Greeting = (props) => {
-  return (
-    <View style={styles.center}>
-      <Text>Hello {props.name}!</Text>
-    </View>
-  );
-}
-
-const LotsOfGreetings = () => {
-  return (
-    <View style={[styles.center, {top: 50}]}>
-      <Greeting name='Rexxar' />
-      <Greeting name='Jaina' />
-      <Greeting name='Valeera' />
-    </View>
-  );
-}
-
-export default LotsOfGreetings;
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
